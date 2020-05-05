ceb65f89646ff31289fab6bac85e6255c72ae686
harumarusuke <harumaruchabin@gmail.com>
2020-05-05 07:30:26 +0000

    管理権限実装後

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   
+  
+  def index
+    @users=User.paginate(page: params[:page])
+  end
+  
+  
   def show
     @user = User.find(params[:id])
   end
@@ -17,6 +23,10 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+  
+  def edit
+    @user=User.find(params[:id])
   end
   
   private
